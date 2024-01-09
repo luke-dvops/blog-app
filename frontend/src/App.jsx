7f0ad8d816b194b26bdb47a680065035bc309d82
luke-dvops <luketan30@gmail.com>
2024-01-09 10:39:20 +0800

    Include untracked files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+	return (
+		<div>
+			<Navbar />
+      <Routes>
+			<Route exact path="/" element={<Home/>}/>
+      </Routes>
+      <Footer/>
+		</div>
+	);
+};
 
-export default App
+export default App;
