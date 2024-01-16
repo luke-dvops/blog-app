5e20f4a8515c620db15d45cde7ac8d377e975737
luke-dvops <luketan30@gmail.com>
2024-01-16 20:54:36 +0800

    Implement login and register functionality

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const connectDB = async () => {
 //middlewares
 dotenv.config();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: ['http://127.0.0.1:5173', 'http://localhost:5173'], credentials: true }));
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
