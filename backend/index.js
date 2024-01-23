e1239ec6f41ee74d820235c14cd7b63418ed10c0
haosen <youremail@example.com>
2024-01-23 08:44:46 +0800

    Implemented the updated backend for Blog with additional functions

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,12 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
 const cookieParser = require("cookie-parser");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
-const commentRoute=require('./routes/comments')
+const commentRoute = require("./routes/comments");
 
 //database
 const connectDB = async () => {
@@ -21,11 +23,29 @@ const connectDB = async () => {
 //middlewares
 dotenv.config();
 app.use(express.json());
-app.use(cors({ origin: ['http://127.0.0.1:5173', 'http://localhost:5173'], credentials: true }));
+app.use("/images", express.static(path.join(__dirname, "/images")));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
-app.use("/api/comments",commentRoute)
+app.use("/api/comments", commentRoute);
+
+//image upload
+const storage = multer.diskStorage({
+  destination: (req, file, fn) => {
+    fn(null, "images");
+  },
+  filename: (req, file, fn) => {
+    fn(null, req.body.img);
+    // fn(null,"image1.jpg")
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  // console.log(req.body)
+  res.status(200).json("Image has been uploaded successfully!");
+});
 
 app.listen(process.env.PORT, () => {
   connectDB();
