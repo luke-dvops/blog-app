4e2e3a53c1e12ca31166179a2c7e7a4b6070f10a
luke-dvops <luketan30@gmail.com>
2024-01-23 16:54:27 +0800

    Made a copy of components & pages, main and App jsx files in instrumented folder

---
+++ frontend/src/instrumented/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import HomePost from "../components/HomePost";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    <div className='px-8 lg:px-[200px]'>
+        <HomePost/>
+        <HomePost/>
+        <HomePost/>
+        <HomePost/>
+        <HomePost/>
+        <HomePost/>
+    </div>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
